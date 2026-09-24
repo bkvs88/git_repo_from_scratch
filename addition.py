b705defcd793a2370bff5853413ce0d129e9fddc
Sravan Kumar Bandaru <bkvs88@gmail.com>
2026-09-25 00:42:16 +0800

    Add addition and substraction operations

---
+++ addition.py
@@ -0,0 +1,5 @@
+from readdata import a,b
+def add(a,b):
+    c=a+b
+    print(f"Addition of {a} and {b} is : {c}")
+    return c
---
+++ substraction.py
@@ -0,0 +1,5 @@
+from readdata import a,b
+def sub(a,b):
+    c=a - b
+    print(f"substraction of {a} and {b} is : {c}")
+    return c
