43c059fc2968c829784bc0d128af13202353362c
Sravan Kumar Bandaru <bkvs88@gmail.com>
2026-09-25 00:42:19 +0800

    Add calculator main entry point that runs all operations

---
+++ calculator.py
@@ -0,0 +1,10 @@
+from readdata import a , b
+from addition import add
+from substraction import sub
+from division import div
+from multiplication import multiply
+print("Basic Calculator")
+add(a,b)
+sub(a,b)
+div(a,b)
+multiply(a,b)
